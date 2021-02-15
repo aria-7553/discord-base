c9b770339aa2fa82ca08609c795bf350001f1146
aria#7553 <wentaas@gmail.com>
2021-02-15 08:06:46 -0800

    added docs

---
+++ doc/implementors/serde/de/trait.Deserialize.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["discord_base"] = [{"text":"impl&lt;'de&gt; Deserialize&lt;'de&gt; for BotConfig","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
